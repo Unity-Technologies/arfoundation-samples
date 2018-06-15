447d3d4f91dc0c06303e7ef4ce5e1a736a74c2ab
Mike Durand <michaeldu@unity3d.com>
2018-06-14 20:21:55 -0700

    Initial push of arfoundation-samples.

---
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class PlaceOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject placedObject { get; private set; }
+
+    void Awake()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        var touch = Input.GetTouch(0);
+
+        var hits = s_RaycastHits;
+        
+        if (!m_SessionOrigin.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+            return;
+
+        placementHit = hits[0];
+    }
+
+    ARSessionOrigin m_SessionOrigin;
+
+    ARRaycastHit m_PlacementHit;
+
+    ARRaycastHit placementHit
+    {
+        get { return m_PlacementHit; }
+        set
+        {
+            m_PlacementHit = value;
+
+            if (placedObject == null && m_PlacedPrefab != null)
+            {
+                placedObject = Instantiate(m_PlacedPrefab);
+            }
+
+            if (placedObject != null)
+            {
+                var pose = m_PlacementHit.pose;
+                placedObject.transform.position = pose.position;
+                placedObject.transform.rotation = pose.rotation;
+            }
+        }
+    }
+
+    List<ARRaycastHit> s_RaycastHits = new List<ARRaycastHit>();
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# arfoundation-samples
-Example content for Unity projects based on AR Foundation
+# AR Foundation Samples
+Example projects that utilize *AR Foundation* and demonstrates its functionality with sample assets and components.
+
+Requires Unity version 2018.1+
+
+Requires the AR Foundation package
+
+## Instructions for installing AR Foundation
+
+1. Download the latest version of Unity 2018.1 or 2018.2
+
+2. Open Unity and load the project at the root of the *arfoundation-samples* repository
+
+3. Open your choice of sample scenes.
+
+4. See the documentation for the AR Foundation for usage instructions and more information
+See the [AR Foundation Documentation](https://docs.unity3d.com/Packages/com.unity.xr.arutilities@1.0/manual/index.html) to learn how to get started with the core components.
