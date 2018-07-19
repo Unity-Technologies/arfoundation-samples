c912c4b2f9a80566f1e9d5b1c20f0908e0e3409d
John Sietsma <john.sietsma@gmail.com>
2018-07-19 16:40:07 +1000

    Added sample for copying the camera background to a RenderTexture. ARCore tested, ARKit to be tested. Note this relies on an unpublished version of ARFoundation (aec674e). Upgraded to Unity 2018.2.0f2.

---
+++ Assets/Scripts/ARCameraBackgroundRenderTexture.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARCameraBackground))]
+public class ARCameraBackgroundRenderTexture : MonoBehaviour {
+
+    public RenderTexture targetRenderTexture;
+
+    private ARCameraBackground m_cameraBackground;
+    private List<Texture2D> m_Textures = new List<Texture2D>();
+
+
+	void Start () {
+        m_cameraBackground = GetComponent<ARCameraBackground>();
+	}
+
+    private void OnEnable()
+    {
+        ARSubsystemManager.cameraFrameReceived += ARSubsystemManager_cameraFrameReceived;
+    }
+
+    private void OnDisable()
+    {
+        ARSubsystemManager.cameraFrameReceived -= ARSubsystemManager_cameraFrameReceived;
+    }
+
+    private void ARSubsystemManager_cameraFrameReceived(ARCameraFrameEventArgs cameraFrameEventArgs)
+    {
+        ARSubsystemManager.cameraSubsystem.GetTextures(m_Textures);
+#if UNITY_ANDROID
+        Graphics.Blit(m_Textures[0], targetRenderTexture, m_cameraBackground.backgroundRenderer.backgroundMaterial);
+#elif UNITY_IOS
+        Material backgroundMaterial = m_cameraBackground.backgroundRenderer.backgroundMaterial;
+        m_ClearMaterial.SetTexture("_textureY", m_Textures[0]);
+        m_ClearMaterial.SetTexture("_textureCbCr", m_Textures[1]);
+        Graphics.Blit(null, targetRenderTexture, backgroundMaterial);
+#endif
+    }
+}
