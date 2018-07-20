057847661d5a297b04d6d6bc6593b14e369087cb
John Sietsma <john.sietsma@gmail.com>
2018-07-20 10:59:06 +1000

    Add ARKit background capture. Restore the package manifest to the correct packages. NOTE: This relies on an unpublished version of ARFoundation, update the package manifest to point to your local copy.

--- Assets/Scripts/ARCameraBackgroundRenderTexture.cs
+++ Assets/Scripts/ARCameraBackgroundRenderTexture.cs
@@ -1,4 +1,3 @@
-﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
@@ -28,13 +27,23 @@ public class ARCameraBackgroundRenderTexture : MonoBehaviour {
 
     private void ARSubsystemManager_cameraFrameReceived(ARCameraFrameEventArgs cameraFrameEventArgs)
     {
+        // NOTE: This relies on the default background material. If you change the default background
+        //    material, then you may have to change the material setup here.
+        
         ARSubsystemManager.cameraSubsystem.GetTextures(m_Textures);
 #if UNITY_ANDROID
+        // On ARCore we require a glsl shader that can access the external sampler.
+        // We'll simply re-use the material that has been set up for the background render.
+        // There is one RBG texture, we'll Blit it to our RenderTexture.
         Graphics.Blit(m_Textures[0], targetRenderTexture, m_cameraBackground.backgroundRenderer.backgroundMaterial);
 #elif UNITY_IOS
+        // On ARKit there are two textures, Y and CbCr. The shader combines these into a single RGB output.
+        // We re-use the existing background material. But we have to set up the two textures first.
         Material backgroundMaterial = m_cameraBackground.backgroundRenderer.backgroundMaterial;
-        m_ClearMaterial.SetTexture("_textureY", m_Textures[0]);
-        m_ClearMaterial.SetTexture("_textureCbCr", m_Textures[1]);
+        backgroundMaterial.SetTexture("_textureY", m_Textures[0]);
+        backgroundMaterial.SetTexture("_textureCbCr", m_Textures[1]);
+        
+        // We don't have a main texture, so just pass in null.
         Graphics.Blit(null, targetRenderTexture, backgroundMaterial);
 #endif
     }
