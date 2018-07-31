cc88d998d1daaee4f499de99a93dd5db650e32e8
Dan <danielmi@unity3d.com>
2018-07-31 14:42:48 -0700

    Added light estimation script and scene with UI to display values

---
+++ Assets/Scripts/LightEstimation.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(Light))]
+public class LightEstimation : MonoBehaviour
+{
+	Light m_Light;
+
+	/// <summary>
+	/// The light affected
+	/// </summary>
+	public Light Light
+	{
+		get { return m_Light; }
+		set { m_Light = value; }
+	}
+
+	float? m_Brightness;
+
+	public float? Brightness
+	{
+		get { return m_Brightness; }
+	}
+
+	float? m_ColorTemperature;
+
+	public float? ColorTemperature
+	{
+		get { return m_ColorTemperature; }
+	}
+
+	Color? m_ColorCorrection;
+
+	public Color? ColorCorrection
+	{
+		get { return m_ColorCorrection; }
+	}
+
+	void Awake ()
+	{
+		m_Light = GetComponent<Light>();
+		ARSubsystemManager.cameraFrameReceived += FrameChanged;
+	}
+
+	void FrameChanged(ARCameraFrameEventArgs args)
+	{
+		if (args.lightEstimation.averageBrightness.HasValue)
+		{
+			m_Brightness = args.lightEstimation.averageBrightness.Value;
+			m_Light.intensity = m_Brightness.Value;
+		}
+
+		if (args.lightEstimation.averageColorTemperature.HasValue)
+		{
+			m_ColorTemperature = args.lightEstimation.averageColorTemperature.Value;
+			m_Light.colorTemperature = m_ColorTemperature.Value;
+		}
+		
+		if (args.lightEstimation.colorCorrection.HasValue)
+		{
+			m_ColorCorrection = args.lightEstimation.colorCorrection.Value;
+			m_Light.color = m_ColorCorrection.Value;
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/LightEstimationUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LightEstimation))]
+public class LightEstimationUI : MonoBehaviour
+{
+	[SerializeField]
+	Text m_BrightnessVal;
+	
+	[SerializeField]
+	Text m_ColorTempVal;
+	
+	[SerializeField]
+	Text m_ColorCorrectVal;
+
+	LightEstimation m_LightEstimation;
+
+	const string k_UnsupportedText = "Not Supported";
+
+	void Awake()
+	{
+		m_LightEstimation = GetComponent<LightEstimation>();
+	}
+
+	void Update()
+	{
+		if (m_LightEstimation.Brightness.HasValue)
+		{
+			m_BrightnessVal.text = m_LightEstimation.Brightness.Value.ToString();
+		}
+		else
+		{
+			m_BrightnessVal.text = k_UnsupportedText;
+		}
+		
+		if (m_LightEstimation.ColorTemperature.HasValue)
+		{
+			m_ColorTempVal.text = m_LightEstimation.ColorTemperature.Value.ToString();
+		}
+		else
+		{
+			m_ColorTempVal.text = k_UnsupportedText;
+		}
+
+		if (m_LightEstimation.ColorCorrection.HasValue)
+		{
+			m_ColorCorrectVal.text = m_LightEstimation.ColorCorrection.Value.ToString();
+		}
+		else
+		{
+			m_ColorCorrectVal.text = k_UnsupportedText;
+		}
+	}
+	
+}
