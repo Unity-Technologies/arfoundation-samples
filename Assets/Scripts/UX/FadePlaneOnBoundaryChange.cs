4c7fddaf5b3279cb4504b13845f5ab486a4dc47d
Tim Mowrer <timm@unity3d.com>
2018-12-14 13:53:31 -0800

    Remove unnecessary usings

--- Assets/Scripts/UX/FadePlaneOnBoundaryChange.cs
+++ Assets/Scripts/UX/FadePlaneOnBoundaryChange.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 
 public class FadePlaneOnBoundaryChange : MonoBehaviour
