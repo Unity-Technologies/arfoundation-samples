6771a79908757180a509ee5d1401bf8bd30c7c7e
Dan <danielmi@unity3d.com>
2018-12-14 13:15:45 -0800

    added UX update, feature point texture, plane texture, UX animations for plane finding and placement, UX for fading planes off

---
+++ Assets/Scripts/PlaceMultipleObjectsOnPlane.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class PlaceMultipleObjectsOnPlane : MonoBehaviour
+{
+	[SerializeField]
+	[Tooltip("Instantiates this prefab on a plane at the touch location.")]
+	GameObject m_PlacedPrefab;
+
+	/// <summary>
+	/// The prefab to instantiate on touch.
+	/// </summary>
+	public GameObject placedPrefab
+	{
+		get { return m_PlacedPrefab; }
+		set { m_PlacedPrefab = value; }
+	}
+    
+	/// <summary>
+	/// The object instantiated as a result of a successful raycast intersection with a plane.
+	/// </summary>
+	public GameObject spawnedObject { get; private set; }
+
+	ARSessionOrigin m_SessionOrigin;
+    
+	static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+	
+	public delegate void PlacedObject();
+	
+	private static Action s_PlacedObject;
+
+	public static event Action onPlacedObject;
+	
+	void Awake()
+	{
+		m_SessionOrigin = GetComponent<ARSessionOrigin>();
+	}
+
+	void Update()
+	{
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if (touch.phase == TouchPhase.Began)
+			{
+				if (m_SessionOrigin.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+				{
+					Pose hitPose = s_Hits[0].pose;
+
+					spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+
+					if (onPlacedObject != null)
+					{
+						onPlacedObject();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/UX/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class UIManager : MonoBehaviour
+{
+
+	const string k_FadeOffAnim = "FadeOff";
+	const string k_FadeOnAnim = "FadeOn";
+
+	[SerializeField] ARPlaneManager m_PlaneManager;
+	[SerializeField] Animator m_MoveDeviceAnimation;
+	[SerializeField] Animator m_TapToPlaceAnimation;
+	
+	List<ARPlane> m_Planes = new List<ARPlane>();
+
+	bool m_ShowingTapToPlace = false;
+	bool m_ShowingMoveDevice = true;
+
+	void OnEnable()
+	{
+		ARSubsystemManager.cameraFrameReceived += FrameChanged;
+		PlaceMultipleObjectsOnPlane.onPlacedObject += PlacedObject;
+	}
+
+	void OnDisable()
+	{
+		ARSubsystemManager.cameraFrameReceived -= FrameChanged;
+		PlaceMultipleObjectsOnPlane.onPlacedObject -= PlacedObject;
+	}
+
+	void FrameChanged(ARCameraFrameEventArgs args)
+	{
+		if (PlanesFound() && m_ShowingMoveDevice)
+		{
+			m_MoveDeviceAnimation.SetTrigger(k_FadeOffAnim);
+			m_TapToPlaceAnimation.SetTrigger(k_FadeOnAnim);
+			m_ShowingTapToPlace = true;
+			m_ShowingMoveDevice = false;
+		}
+	}
+
+	bool PlanesFound()
+	{
+		if (m_PlaneManager)
+		{
+			m_PlaneManager.GetAllPlanes(m_Planes);
+			return m_Planes.Count > 0;
+		}
+		return false;
+	}
+
+	void PlacedObject()
+	{
+		if (m_ShowingTapToPlace)
+		{
+			m_TapToPlaceAnimation.SetTrigger(k_FadeOffAnim);
+			m_ShowingTapToPlace = false;
+		}
+	}
+
+}
