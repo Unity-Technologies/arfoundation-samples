fc79b5a6518bf8430e890465dbc04d3989649b1e
Tim Mowrer <timm@unity3d.com>
2018-12-14 13:40:34 -0800

    Convert tabs to spaces

--- Assets/Scripts/UX/FadePlaneOnBoundaryChange.cs
+++ Assets/Scripts/UX/FadePlaneOnBoundaryChange.cs
@@ -5,49 +5,49 @@ using UnityEngine.XR.ARFoundation;
 
 public class FadePlaneOnBoundaryChange : MonoBehaviour
 {
-	const string k_FadeOffAnim = "FadeOff";
-	const string k_FadeOnAnim = "FadeOn";
-	const float k_TimeOut = 2.0f;
-	
-	Animator m_Animator;
-	ARPlane m_Plane;
+    const string k_FadeOffAnim = "FadeOff";
+    const string k_FadeOnAnim = "FadeOn";
+    const float k_TimeOut = 2.0f;
+    
+    Animator m_Animator;
+    ARPlane m_Plane;
 
-	float m_ShowTime = 0;
-	bool m_UpdatingPlane = false;
+    float m_ShowTime = 0;
+    bool m_UpdatingPlane = false;
 
-	void OnEnable()
-	{
-		m_Plane = GetComponent<ARPlane>();
-		m_Animator = GetComponent<Animator>();
-		
-		m_Plane.boundaryChanged += PlaneOnBoundaryChanged;
-	}
+    void OnEnable()
+    {
+        m_Plane = GetComponent<ARPlane>();
+        m_Animator = GetComponent<Animator>();
+        
+        m_Plane.boundaryChanged += PlaneOnBoundaryChanged;
+    }
 
-	void OnDisable()
-	{
-		m_Plane.boundaryChanged -= PlaneOnBoundaryChanged;
-	}
+    void OnDisable()
+    {
+        m_Plane.boundaryChanged -= PlaneOnBoundaryChanged;
+    }
 
-	void Update()
-	{
-		if (m_UpdatingPlane)
-		{
-			m_ShowTime -= Time.deltaTime;
+    void Update()
+    {
+        if (m_UpdatingPlane)
+        {
+            m_ShowTime -= Time.deltaTime;
 
-			if (m_ShowTime <= 0)
-			{
-				m_UpdatingPlane = false;
-				m_Animator.SetBool(k_FadeOffAnim, true);
-				m_Animator.SetBool(k_FadeOnAnim, false);
-			}
-		}
-	}
+            if (m_ShowTime <= 0)
+            {
+                m_UpdatingPlane = false;
+                m_Animator.SetBool(k_FadeOffAnim, true);
+                m_Animator.SetBool(k_FadeOnAnim, false);
+            }
+        }
+    }
 
-	void PlaneOnBoundaryChanged(ARPlaneBoundaryChangedEventArgs obj)
-	{
-		m_Animator.SetBool(k_FadeOffAnim, false);
-		m_Animator.SetBool(k_FadeOnAnim, true);
-		m_UpdatingPlane = true;
-		m_ShowTime = k_TimeOut;
-	}
+    void PlaneOnBoundaryChanged(ARPlaneBoundaryChangedEventArgs obj)
+    {
+        m_Animator.SetBool(k_FadeOffAnim, false);
+        m_Animator.SetBool(k_FadeOnAnim, true);
+        m_UpdatingPlane = true;
+        m_ShowTime = k_TimeOut;
+    }
 }
--- Assets/Scripts/UX/UIManager.cs
+++ Assets/Scripts/UX/UIManager.cs
@@ -5,59 +5,58 @@ using UnityEngine.XR.ARFoundation;
 
 public class UIManager : MonoBehaviour
 {
+    const string k_FadeOffAnim = "FadeOff";
+    const string k_FadeOnAnim = "FadeOn";
 
-	const string k_FadeOffAnim = "FadeOff";
-	const string k_FadeOnAnim = "FadeOn";
+    [SerializeField] ARPlaneManager m_PlaneManager;
+    [SerializeField] Animator m_MoveDeviceAnimation;
+    [SerializeField] Animator m_TapToPlaceAnimation;
+    
+    List<ARPlane> m_Planes = new List<ARPlane>();
 
-	[SerializeField] ARPlaneManager m_PlaneManager;
-	[SerializeField] Animator m_MoveDeviceAnimation;
-	[SerializeField] Animator m_TapToPlaceAnimation;
-	
-	List<ARPlane> m_Planes = new List<ARPlane>();
+    bool m_ShowingTapToPlace = false;
+    bool m_ShowingMoveDevice = true;
 
-	bool m_ShowingTapToPlace = false;
-	bool m_ShowingMoveDevice = true;
+    void OnEnable()
+    {
+        ARSubsystemManager.cameraFrameReceived += FrameChanged;
+        PlaceMultipleObjectsOnPlane.onPlacedObject += PlacedObject;
+    }
 
-	void OnEnable()
-	{
-		ARSubsystemManager.cameraFrameReceived += FrameChanged;
-		PlaceMultipleObjectsOnPlane.onPlacedObject += PlacedObject;
-	}
+    void OnDisable()
+    {
+        ARSubsystemManager.cameraFrameReceived -= FrameChanged;
+        PlaceMultipleObjectsOnPlane.onPlacedObject -= PlacedObject;
+    }
 
-	void OnDisable()
-	{
-		ARSubsystemManager.cameraFrameReceived -= FrameChanged;
-		PlaceMultipleObjectsOnPlane.onPlacedObject -= PlacedObject;
-	}
+    void FrameChanged(ARCameraFrameEventArgs args)
+    {
+        if (PlanesFound() && m_ShowingMoveDevice)
+        {
+            m_MoveDeviceAnimation.SetTrigger(k_FadeOffAnim);
+            m_TapToPlaceAnimation.SetTrigger(k_FadeOnAnim);
+            m_ShowingTapToPlace = true;
+            m_ShowingMoveDevice = false;
+        }
+    }
 
-	void FrameChanged(ARCameraFrameEventArgs args)
-	{
-		if (PlanesFound() && m_ShowingMoveDevice)
-		{
-			m_MoveDeviceAnimation.SetTrigger(k_FadeOffAnim);
-			m_TapToPlaceAnimation.SetTrigger(k_FadeOnAnim);
-			m_ShowingTapToPlace = true;
-			m_ShowingMoveDevice = false;
-		}
-	}
+    bool PlanesFound()
+    {
+        if (m_PlaneManager)
+        {
+            m_PlaneManager.GetAllPlanes(m_Planes);
+            return m_Planes.Count > 0;
+        }
+        return false;
+    }
 
-	bool PlanesFound()
-	{
-		if (m_PlaneManager)
-		{
-			m_PlaneManager.GetAllPlanes(m_Planes);
-			return m_Planes.Count > 0;
-		}
-		return false;
-	}
-
-	void PlacedObject()
-	{
-		if (m_ShowingTapToPlace)
-		{
-			m_TapToPlaceAnimation.SetTrigger(k_FadeOffAnim);
-			m_ShowingTapToPlace = false;
-		}
-	}
+    void PlacedObject()
+    {
+        if (m_ShowingTapToPlace)
+        {
+            m_TapToPlaceAnimation.SetTrigger(k_FadeOffAnim);
+            m_ShowingTapToPlace = false;
+        }
+    }
 
 }
