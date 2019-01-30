30c564680aff340e277e577f8dfaf51c0d4f304f
Tim Mowrer <timm@unity3d.com>
2019-01-29 23:45:23 -0800

    Add a sample showing how to place content with scale

---
+++ Assets/Scripts/MakeAppearOnPlane.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class MakeAppearOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_Content;
+
+    public Transform content
+    {
+        get { return m_Content; }
+        set { m_Content = value; }
+    }
+
+    [SerializeField]
+    Quaternion m_Rotation;
+
+    public Quaternion rotation
+    {
+        get { return m_Rotation; }
+        set
+        {
+            m_Rotation = value;
+            if (m_SessionOrigin != null)
+                m_SessionOrigin.MakeContentAppearAt(content, content.transform.position, m_Rotation);
+        }
+    }
+
+    void Awake()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0 || m_Content == null)
+            return;
+
+        var touch = Input.GetTouch(0);
+
+        if (m_SessionOrigin.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+
+            m_SessionOrigin.MakeContentAppearAt(content, hitPose.position, m_Rotation);
+        }
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    ARSessionOrigin m_SessionOrigin;
+}
---
+++ Assets/Scripts/ScaleController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+[RequireComponent(typeof(ARSessionOrigin))]
+public class ScaleController : MonoBehaviour
+{
+    [SerializeField]
+    Slider m_Slider;
+
+    public Slider slider
+    {
+        get { return m_Slider; }
+        set { m_Slider = value; }
+    }
+
+    [SerializeField]
+    public float m_Min = .1f;
+
+    public float min
+    {
+        get { return m_Min; }
+        set { m_Min = value; }
+    }
+
+    [SerializeField]
+    public float m_Max = 10f;
+
+    public float max
+    {
+        get { return m_Max; }
+        set { m_Max = value; }
+    }
+
+    public void SetScale()
+    {
+        if (slider == null)
+            return;
+
+        float scale = slider.value * (max - min) + min;
+        m_SessionOrigin.transform.localScale = Vector3.one * scale;
+    }
+
+    void Awake()
+    {
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+    }
+
+    void OnEnable()
+    {
+        if (slider != null)
+        {
+            var scale = m_SessionOrigin.transform.localScale.x;
+            slider.value = (scale - min) / (max - min);
+        }
+    }
+
+    ARSessionOrigin m_SessionOrigin;
+}
