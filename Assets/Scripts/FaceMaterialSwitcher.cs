cbb80879818ebf0ca15d4a8dc66b1779ac7b6180
Nishant Naveen <nishant.naveen@unity3d.com>
2019-06-12 13:52:21 -0700

    Update FaceMesh scene to use a different face mesh material for faces (#2)
    * Update FaceMesh scene to use a different face mesh material for detected faces

---
+++ Assets/Scripts/FaceMaterialSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class FaceMaterialSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Materials to use for face meshes.")]
+    Material[] m_FaceMaterials;
+
+    /// <summary>
+    /// Getter/setter for the Face Materials.
+    /// </summary>
+    public Material[] faceMaterials
+    {
+        get { return m_FaceMaterials; }
+        set { m_FaceMaterials = value; }
+    }
+
+    static int s_CurrentMaterialIndex;
+    static Dictionary<TrackableId, Material> s_FaceTracker = new Dictionary<TrackableId, Material>();
+
+    void Start()
+    {
+        ARFace face = GetComponent<ARFace>();
+        Material mat;
+        if (!s_FaceTracker.TryGetValue(face.trackableId, out mat))
+        {
+            s_FaceTracker.Add(face.trackableId, m_FaceMaterials[s_CurrentMaterialIndex]);
+            GetComponent<MeshRenderer>().material = m_FaceMaterials[s_CurrentMaterialIndex];
+            s_CurrentMaterialIndex = (s_CurrentMaterialIndex + 1) % m_FaceMaterials.Length;
+        }
+        else
+        {
+            // Assign the material that was already used for the face's unique id.
+            GetComponent<MeshRenderer>().material = mat;
+        }
+    }
+}
