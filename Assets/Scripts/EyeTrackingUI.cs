2d6b040a3eea614d66b693e8fcb07cb3eca9bff2
David Mohrhardt <davidmo@unity3d.com>
2019-08-05 15:04:29 -0700

    Update the scenes with smaller text to not overflow the screen when not supported.

--- Assets/Scripts/EyeTrackingUI.cs
+++ Assets/Scripts/EyeTrackingUI.cs
@@ -5,6 +5,7 @@ using Text = UnityEngine.UI.Text;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
+[RequireComponent(typeof(Text))]
 public class EyeTrackingUI : MonoBehaviour
 {
     [SerializeField]
@@ -16,10 +17,15 @@ public class EyeTrackingUI : MonoBehaviour
         {
             m_Manager = FindObjectOfType<ARFaceManager>();
         }
-        if (m_Manager != null && m_Manager.subsystem.SubsystemDescriptor.supportsEyeTracking)
+        if (m_Manager != null && m_Manager.subsystem != null && m_Manager.subsystem.SubsystemDescriptor.supportsEyeTracking)
         {
             var infoGO = GetComponent<Text>();
             infoGO.text = "This device supports eye tracking.";
+        }
+        else
+        {
+            var infoGO = GetComponent<Text>();
+            infoGO.text = "This device does not support eye tracking.";
         }
     }
 }
