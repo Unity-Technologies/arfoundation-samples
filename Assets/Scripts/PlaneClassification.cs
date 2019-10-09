d2edadfd8f4019e4dbad36e013f1d4ee1d0a4381
Mike Durand <michaeldu@unity3d.com>
2019-10-09 14:56:10 -0700

    -Added sample to demonstrate plane classification

---
+++ Assets/Scripts/PlaneClassification.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaneClassification : MonoBehaviour
+{
+    ARPlaneManager m_ARPlaneManager;
+
+    void Awake()
+    {
+        m_ARPlaneManager = GetComponent<ARPlaneManager>();
+    }
+
+    void OnEnable()
+    {
+        m_ARPlaneManager.planesChanged += OnPlanesAdded;
+        m_ARPlaneManager.planesChanged += OnPlanesUpdated;
+        m_ARPlaneManager.planesChanged += OnPlanesRemoved;
+    }
+
+    void OnDisable()
+    {
+        m_ARPlaneManager.planesChanged -= OnPlanesAdded;
+        m_ARPlaneManager.planesChanged -= OnPlanesUpdated;
+        m_ARPlaneManager.planesChanged -= OnPlanesRemoved;
+    }
+
+    void OnPlanesAdded(ARPlanesChangedEventArgs eventArgs)
+    {
+        foreach (var plane in eventArgs.added)
+        {
+            GameObject textObj = new GameObject();
+            textObj.transform.SetParent(plane.gameObject.transform, false);
+
+            TextMesh planeText = textObj.AddComponent<TextMesh>();
+
+            if (planeText)
+            {
+                planeText.characterSize = 0.05f;
+                planeText.text = plane.classification.ToString();
+            }
+        }
+    }
+
+    void OnPlanesUpdated(ARPlanesChangedEventArgs eventArgs)
+    {
+        foreach (var plane in eventArgs.updated)
+        {
+            TextMesh planeText = plane.GetComponentInChildren<TextMesh>();
+            if (planeText)
+            {
+                planeText.text = plane.classification.ToString();
+            }
+        }
+    }
+
+    void OnPlanesRemoved(ARPlanesChangedEventArgs eventArgs)
+    {
+        foreach (var plane in eventArgs.removed)
+        {
+            Component[] textMeshes;
+
+            textMeshes = plane.gameObject.GetComponentsInChildren(typeof(TextMesh));
+            
+            foreach (TextMesh planeText in textMeshes)
+            {
+                Destroy(planeText.gameObject);
+            }
+        }
+    }
+}
