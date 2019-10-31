9f903116cceda9a60e33be8891df3ca77b1da75b
Mike Durand <michaeldu@unity3d.com>
2019-10-31 16:34:27 -0700

    Renamed all files that included `ReferencePoint` to instead include `Anchor`.

---
+++ Assets/Scripts/AnchorCreator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARAnchorManager))]
+[RequireComponent(typeof(ARRaycastManager))]
+public class AnchorCreator : MonoBehaviour
+{
+    public void RemoveAllAnchors()
+    {
+        foreach (var anchor in m_Anchors)
+        {
+            m_AnchorManager.RemoveAnchor(anchor);
+        }
+        m_Anchors.Clear();
+    }
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+        m_AnchorManager = GetComponent<ARAnchorManager>();
+        m_Anchors = new List<ARAnchor>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount == 0)
+            return;
+
+        var touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began)
+            return;
+
+        if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.FeaturePoint))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            var hitPose = s_Hits[0].pose;
+            var anchor = m_AnchorManager.AddAnchor(hitPose);
+            if (anchor == null)
+            {
+                Logger.Log("Error creating anchor");
+            }
+            else
+            {
+                m_Anchors.Add(anchor);
+            }
+        }
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    List<ARAnchor> m_Anchors;
+
+    ARRaycastManager m_RaycastManager;
+
+    ARAnchorManager m_AnchorManager;
+}
