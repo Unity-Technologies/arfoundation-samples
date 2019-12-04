8b8d275caeeec4402768416b4a8ef4e78db20fbc
David Mohrhardt <davidmo@unity3d.com>
2019-12-03 16:48:23 -0800

    - Added new ARKit HDR light estimation sample scene - Modified LightEstimation scene to display HDR estimation information.

--- Assets/Scripts/LightEstimationUI.cs
+++ Assets/Scripts/LightEstimationUI.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+using SphericalHarmonicsL2 = UnityEngine.Rendering.SphericalHarmonicsL2;
+
 /// <summary>
 /// A simple UI controller to display light estimation information.
 /// </summary>
@@ -37,6 +39,42 @@ public class LightEstimationUI : MonoBehaviour
     [SerializeField]
     Text m_ColorCorrectionText;
 
+    [Tooltip("The UI Text element used to display the estimated direction of the main light for the physical environment.")]
+    [SerializeField]
+    Text m_MainLightDirectionText;
+    public Text mainLightDirectionText
+    {
+        get => m_MainLightDirectionText;
+        set => m_MainLightDirectionText = value;
+    }
+
+    [Tooltip("The UI Text element used to display the estimated intensity in lumens of the main light for the physical environment.")]
+    [SerializeField]
+    Text m_MainLightIntensityLumens;
+    public Text mainLightIntensityLumens
+    {
+        get => m_MainLightIntensityLumens;
+        set => m_MainLightIntensityLumens = value;
+    }
+
+    [Tooltip("The UI Text element used to display the estimated color of the main light for the physical environment.")]
+    [SerializeField]
+    Text m_MainLightColor;
+    public Text mainLightColorText
+    {
+        get => m_MainLightColor;
+        set => m_MainLightColor = value;
+    }
+
+    [Tooltip("The UI Text element used to display the estimated spherical harmonics coefficients for the physical environment.")]
+    [SerializeField]
+    Text m_SphericalHarmonicsText;
+    public Text ambientSphericalHarmonicsText
+    {
+        get => m_SphericalHarmonicsText;
+        set => m_SphericalHarmonicsText = value;
+    }
+
     /// <summary>
     /// The UI Text element used to display the estimated color correction value for the scene.
     /// </summary>
@@ -56,6 +94,43 @@ public class LightEstimationUI : MonoBehaviour
         SetUIValue(m_LightEstimation.brightness, brightnessText);
         SetUIValue(m_LightEstimation.colorTemperature, colorTemperatureText);
         SetUIValue(m_LightEstimation.colorCorrection, colorCorrectionText);
+        SetUIValue(m_LightEstimation.mainLightDirection, mainLightDirectionText);
+        SetUIValue(m_LightEstimation.mainLightColor, mainLightColorText);
+        SetUIValue(m_LightEstimation.mainLightIntensityLumens, mainLightIntensityLumens);
+        SetSphericalHarmonicsUIValue(m_LightEstimation.sphericalHarmonics, ambientSphericalHarmonicsText);
+    }
+
+    void SetSphericalHarmonicsUIValue(SphericalHarmonicsL2? maybeAmbientSphericalHarmonics, Text text)
+    {
+        if (text != null)
+        {
+            if (maybeAmbientSphericalHarmonics.HasValue)
+            {
+                string sphericalHarmonicsCoefficientsStr = "";
+                if (m_LightEstimation.sphericalHarmonics.HasValue)
+                {
+                    for (int i = 0; i < 3; ++i)
+                    {
+                        if (i == 0)
+                            sphericalHarmonicsCoefficientsStr += $"R: [";
+                        else if (i == 1)
+                            sphericalHarmonicsCoefficientsStr += $"G: [";
+                        else
+                            sphericalHarmonicsCoefficientsStr += $"B: [";
+                        
+                        for (int j = 0; j < 9; ++j)
+                        {
+                            sphericalHarmonicsCoefficientsStr += $"{m_LightEstimation.sphericalHarmonics.Value[i,j].ToString("0.000")}{(j != 8 ? ", " : "]\n")}";
+                        }
+                    }
+                }
+                text.text = sphericalHarmonicsCoefficientsStr;
+            }
+            else
+            {
+                text.text = k_UnavailableText;
+            }
+        }
     }
 
     void SetUIValue<T>(T? displayValue, Text text) where T : struct
