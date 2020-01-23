dddb94b432eda59c5edc423ffbff4ba0ea92cea8
Tim Mowrer <timm@unity3d.com>
2020-01-23 00:03:28 -0800

    Update face tracking with rear camera sample

--- Assets/Scenes/FaceTracking/DisplayFaceInfo.cs
+++ Assets/Scenes/FaceTracking/DisplayFaceInfo.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Text;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
+[RequireComponent(typeof(ARSession))]
 [RequireComponent(typeof(ARFaceManager))]
+[RequireComponent(typeof(ARSessionOrigin))]
 public class DisplayFaceInfo : MonoBehaviour
 {
     [SerializeField]
@@ -12,24 +15,102 @@ public class DisplayFaceInfo : MonoBehaviour
 
     public Text faceInfoText
     {
-        get { return m_FaceInfoText; }
-        set { m_FaceInfoText = value; }
+        get => m_FaceInfoText;
+        set => m_FaceInfoText = value;
     }
 
+    [SerializeField]
+    Text m_InstructionsText;
+
+    public Text instructionsText
+    {
+        get => m_InstructionsText;
+        set => m_InstructionsText = value;
+    }
+
+    ARSession m_Session;
+
     ARFaceManager m_FaceManager;
+
+    ARCameraManager m_CameraManager;
+
+    StringBuilder m_Info = new StringBuilder();
+
+    bool m_FaceTrackingSupported;
+
+    bool m_FaceTrackingWithWorldCameraSupported;
 
     void Awake()
     {
         m_FaceManager = GetComponent<ARFaceManager>();
+        m_Session = GetComponent<ARSession>();
+        m_CameraManager = GetComponent<ARSessionOrigin>().camera?.GetComponent<ARCameraManager>();
+    }
+
+    void OnEnable()
+    {
+        // Detect face tracking with world-facing camera support
+        var subsystem = m_Session?.subsystem;
+        if (subsystem != null)
+        {
+            var configs = subsystem.GetConfigurationDescriptors(Allocator.Temp);
+            if (configs.IsCreated)
+            {
+                using (configs)
+                {
+                    foreach (var config in configs)
+                    {
+                        if (config.capabilities.All(Feature.FaceDetection))
+                        {
+                            m_FaceTrackingSupported = true;
+                        }
+
+                        if (config.capabilities.All(Feature.WorldFacingCamera | Feature.FaceDetection))
+                        {
+                            m_FaceTrackingWithWorldCameraSupported = true;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     void Update()
     {
-        if (m_FaceManager.subsystem != null && faceInfoText != null)
+        m_Info.Clear();
+
+        if (m_FaceManager.subsystem != null)
         {
-            faceInfoText.text = $"Supported number of tracked faces: {m_FaceManager.supportedFaceCount}\n" +
-                                $"Max number of faces to track: {m_FaceManager.currentMaximumFaceCount}\n" +
-                                $"Number of tracked faces: {m_FaceManager.trackables.count}";
+            m_Info.Append($"Supported number of tracked faces: {m_FaceManager.supportedFaceCount}\n");
+            m_Info.Append($"Max number of faces to track: {m_FaceManager.currentMaximumFaceCount}\n");
+            m_Info.Append($"Number of tracked faces: {m_FaceManager.trackables.count}\n");
+        }
+
+        if (m_CameraManager)
+        {
+            m_Info.Append($"Requested camera facing direction: {m_CameraManager.requestedFacingDirection}\n");
+            m_Info.Append($"Current camera facing direction: {m_CameraManager.currentFacingDirection}\n");
+        }
+
+        if (!m_FaceTrackingSupported)
+        {
+            if (m_InstructionsText)
+            {
+                m_InstructionsText.text = "Face tracking is not supported.";
+            }
+            else
+            {
+                m_Info.Append("Face tracking is not supported.");
+            }
+        }
+        else if (m_CameraManager.requestedFacingDirection == CameraFacingDirection.World && !m_FaceTrackingWithWorldCameraSupported)
+        {
+            m_Info.Append("Face tracking in world facing camera mode is not supported.");
+        }
+
+        if (m_FaceInfoText)
+        {
+            m_FaceInfoText.text = m_Info.ToString();
         }
     }
 }
---
+++ Assets/Scenes/FaceTracking/ToggleCameraFacingDirection.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ToggleCameraFacingDirection : MonoBehaviour
+{
+    [SerializeField]
+    ARCameraManager m_CameraManager;
+
+    public ARCameraManager cameraManager
+    {
+        get => m_CameraManager;
+        set => m_CameraManager = value;
+    }
+
+    [SerializeField]
+    ARSession m_Session;
+
+    public ARSession session
+    {
+        get => m_Session;
+        set => m_Session = value;
+    }
+
+    void Update()
+    {
+        if (m_CameraManager == null || m_Session == null)
+            return;
+
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            if (m_CameraManager.currentFacingDirection == CameraFacingDirection.User)
+            {
+                m_CameraManager.requestedFacingDirection = CameraFacingDirection.World;
+                m_Session.requestedTrackingMode = TrackingMode.PositionAndRotation;
+            }
+            else
+            {
+                m_CameraManager.requestedFacingDirection = CameraFacingDirection.User;
+                m_Session.requestedTrackingMode = TrackingMode.RotationOnly;
+            }
+        }
+    }
+}
