20e265ec2a3f27bb7d82e20d656fc6f87cd187e3
Alexandra Serralta <alexandraserralta@yahoo.com>
2020-05-01 13:08:44 -0700

    Camera Grain

---
+++ Assets/Scenes/CameraGrain/CameraGrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class CameraGrain: MonoBehaviour
+{
+    ARCameraManager m_CameraManager;
+    Renderer m_Renderer;
+
+    void Start()
+    {
+        m_CameraManager = FindObjectOfType (typeof(ARCameraManager)) as ARCameraManager;
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if(m_Renderer != null && m_CameraManager.cameraGrainTexture != null)
+        {
+            m_Renderer.material.SetTexture("_NoiseTex", m_CameraManager.cameraGrainTexture);
+            m_Renderer.material.SetFloat("_NoiseIntensity", m_CameraManager.noiseIntensity);
+        }
+    }
+
+}
