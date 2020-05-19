ee38eb28d21203bafa50b4d3173968b796f80296
Tim Mowrer <timm@unity3d.com>
2020-05-19 11:03:34 -0700

    Add clarifying comments to the test script and use new NativeSlice API

--- Assets/Scripts/TestCameraImage.cs
+++ Assets/Scripts/TestCameraImage.cs
@@ -207,21 +207,33 @@ public class TestCameraImage : MonoBehaviour
 
     static void UpdateRawImage(RawImage rawImage, XRCpuImage cpuImage)
     {
+        // Get the texture associated with the UI.RawImage that we wish to display on screen.
         var texture = rawImage.texture as Texture2D;
+
+        // If the texture hasn't yet been created, or if its dimensions have changed, (re)create the texture.
+        // Note: Although texture dimensions do not normally change frame-to-frame, they can change in response to
+        //    a change in the camera resolution (for camera images) or changes to the quality of the human depth
+        //    and human stencil buffers.
         if (texture == null || texture.width != cpuImage.width || texture.height != cpuImage.height)
         {
             texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
             rawImage.texture = texture;
         }
 
+        // For display, we need to mirror about the vertical access.
         var conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorY);
+
+        // Get the Texture2D's underlying pixel buffer.
         var rawTextureData = texture.GetRawTextureData<byte>();
 
-        unsafe
-        {
-            cpuImage.Convert(conversionParams, new IntPtr(rawTextureData.GetUnsafePtr()), rawTextureData.Length);
-        }
+        // Make sure the destination buffer is large enough to hold the converted data (they should be the same size)
+        Debug.Assert(rawTextureData.Length == cpuImage.GetConvertedDataSize(conversionParams.outputDimensions, conversionParams.outputFormat),
+            "The Texture2D is not the same size as the converted data.");
 
+        // Perform the conversion.
+        cpuImage.Convert(conversionParams, rawTextureData);
+
+        // "Apply" the new pixel data to the Texture2D.
         texture.Apply();
     }
 
