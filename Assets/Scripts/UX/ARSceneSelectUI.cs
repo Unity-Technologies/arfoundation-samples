f90c043d29aa1675fb50a031f21801f3986606bc
Tim Mowrer <timm@unity3d.com>
2020-05-29 11:56:09 -0700

    Add using directive

--- Assets/Scripts/UX/ARSceneSelectUI.cs
+++ Assets/Scripts/UX/ARSceneSelectUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.ARFoundation;
 
 public class ARSceneSelectUI : MonoBehaviour
 {
